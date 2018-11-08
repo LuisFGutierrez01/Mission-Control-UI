3feaa9880f1f6abe4bf526da07b0b5474c7adb19
Luis Gutierrez <luis.f.gutierrez01@utrgv.edu>
2018-11-08 11:48:25 -0600

    Added the pages links and added dropdown. TODO dropbar.

--- app.py
+++ app.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route("/demo")
+def demo():
+    return render_template('demo.html')
+
+@app.route("/info")
+def info():
+    return render_template('info.html')
+
+@app.route("/stream")
+def stream():
+    return render_template('stream.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
